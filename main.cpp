01d89de4445525ad60fc2251e1e9e4ff4b5f1d56
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-05-10 17:03:23 -0600

    Refactor graph rendering to switch output format from SVG to DOT

--- main.cpp
+++ main.cpp
@@ -104,8 +104,8 @@ public:
 
         gvLayout(gvc, g, "sfdp");
         //gvRenderFilename(gvc, g, "png", "grafo.png");
-        gvRenderFilename(gvc, g, "svg", "grafo.svg");
-        //gvRenderFilename(gvc, g, "dot", "grafo.dot");
+        //gvRenderFilename(gvc, g, "svg", "grafo.svg");
+        gvRenderFilename(gvc, g, "dot", "grafo.dot");
 
         gvFreeLayout(gvc, g);
         agclose(g);
